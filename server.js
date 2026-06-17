31ea5f652a9b0a5fceca0f497d357cf023ea4f2a
AdiLakshmi <adilakshmigaddam8@gmail.com>
2026-06-17 18:59:46 +0530

    Fix CORS

--- server.js
+++ server.js
@@ -89,7 +89,6 @@ const allowedOrigins = [
   'http://127.0.0.1:5500',
   'http://127.0.0.1:3000',
   'http://127.0.0.1:5000',
-  'https://acdscore-3.onrender.com',
   'https://acdscore.vercel.app',
 ].filter(Boolean);
 
